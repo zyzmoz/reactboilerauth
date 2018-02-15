44dc1f3b4de548d364ee32dccd85538882ea097d
Daniel Cunha <danielbrandaodacunha@gmail.com>
2018-02-15 15:33:23 -0200

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { 
+    BrowserRouter as Router, 
+    Route
+} from 'react-router-dom';
+
+import Navigation from './Navigation';
+import * as routes from '../constants/routes';
+
+//** Import Pages */
+import LandingPage from '../pages/Landing';
+import SignUpPage from '../pages/SignUp';
+import SignInPage from '../pages/SignIn';
+import PasswordForgetPage from '../pages/PasswordForget';
+import HomePage from '../pages/Home';
+import AccountPage from '../pages/Account';
+
+const App = () => 
+    <Router>
+        <Navigation />
+        <hr/>
+        <Route
+            exact path={routes.LANDING}
+            component={() => <LandingPage />}
+        />
+        <Route
+            exact path={routes.SIGN_UP}
+            component={() => <SignUpPage />}
+        />
+        <Route
+            exact path={routes.SIGN_IN}
+            component={() => <SignInPage />}
+        />
+        <Route
+            exact path={routes.PASSWORD_FORGET}
+            component={() => <PasswordForgetPage />}
+        />
+        <Route
+            exact path={routes.HOME}
+            component={() => <HomePage />}
+        />
+        <Route
+            exact path={routes.ACCOUNT}
+            component={() => <AccountPage />}
+        />
+    </Router>
+
+export default App;
---
+++ src/pages/PasswordForget.js
@@ -0,0 +1,8 @@
+import React from 'rect';
+
+const PasswordForgetPage = () =>
+    <div>
+        <h1>Forget Password Page</h1>
+    </div>
+
+export default PasswordForgetPage;
